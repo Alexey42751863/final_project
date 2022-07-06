02642f0934a087a2e1aee5ad37d800dd6fab086a
Alexey <gasparyanalexey@gmail.com>
2022-07-06 16:38:57 +0400

    merge Shushan

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,36 +1,42 @@
 import React from 'react'
-import {NavLink as Link} from 'react-router-dom'
-import { useState } from 'react'
+import { NavLink as Link } from 'react-router-dom'
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const Navbar = () => {
- const [open, setOpen] = useState(true)
+  const [open, setOpen] = useState(false)
+  const showSideBar = () => setOpen(true)
 
-
+  useEffect(() => {
+    document.addEventListener("mousedown", () => {
+      setOpen(false)
+    })
+  })
   return (
     <>
-     
-      <div className='bars' onClick={() => setOpen(!open)}>
-       <i className="fa-solid fa-bars"></i>
-       </div>
+      <div className='bars' onClick={showSideBar}>
+        <i className="fa-solid fa-bars"></i>
+      </div>
       <div className={open ? "container open" : "container"}>
-      <span className='user'>
-      <i className="fa-solid fa-circle-user"></i>
-      <h2>Մուտք</h2>
-      </span>
-     
+        <nav className="navbar">
+          <span className='user'>
+            <i className="fa-solid fa-circle-user"></i>
+            <h2>Մուտք</h2>
 
-      <nav className="navbar">
-      <Link to={'/'} className="home">Home</Link>
-      <Link to={'/dealers'} className="dillers" >Դիլերներ</Link>
-      <Link to={'/be_a_dealer'} className="avtodiller">Դառնալ ավտոդիլեր</Link>
-      <Link to={'/advertising'} className="govazd">Գովազդ կայքում</Link>
-      <Link to={'/terms_of_use'} className="kanon">Օգտագործման կանոնները</Link>
-      <Link to={'/help'} className="help">Օգնություն</Link>
-      <Link to={'/contact'} className="call">Կապ</Link>  
-    </nav>
-    </div>
+          </span>
+          <i className="fa-solid fa-message">հաղորդագրություններ</i>
+
+          <Link to={'/dealers'} className="dillers" >Դիլերներ</Link>
+          <Link to={'/be_a_dealer'} className="avtodiller">Դառնալ ավտոդիլեր</Link>
+          <Link to={'/advertising'} className="govazd">Գովազդ կայքում</Link>
+          <Link to={'/terms_of_use'} className="kanon">Օգտագործման կանոնները</Link>
+          <Link to={'/help'} className="help">Օգնություն</Link>
+          <Link to={'/contact'} className="call">Կապ</Link>
+        </nav>
+      </div>
     </>
+
   )
 
-  }
+}
 export default Navbar
