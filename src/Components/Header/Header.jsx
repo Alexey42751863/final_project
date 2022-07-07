8300ac2a01740f21b6822a72fc7ba78691250755
shushan-melkumyan <shushanmelkumyan93@gmail.com>
2022-07-07 21:10:08 +0300

    fixed messages

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,21 +1,44 @@
 import React from 'react'
 import "./header.css";
-import Navbar from '../Navbar'
-import { useEffect } from "react";
+import Navbar from '../Navbar';
+import { useState } from 'react';
+import { useEffect } from 'react';
 const Header = () => {
+
+  const [openM,setOpenM] = useState(false)
+  const showSideBarM = () => setOpenM(true)
+
+  useEffect(() => {
+  let handlerM =  (event) =>{
+    if (event.target.className == 'massageBar open') {
+     
+      setOpenM(false)
+    }
+
+  }
+  document.addEventListener("mousedown",handlerM)
+  return () => {
+    document.removeEventListener("mousedown",handlerM)
+  }
+})
   return (
+    
      <div className='header'>
-      <div className="menu">
-      <Navbar/>
-      <a href=""><span>avto</span>.am</a>
-      </div>
-        
-      
+        <Navbar/>
+        <h2><a href="">avto.am</a></h2>
         <div className='diler'>
           <i className="fa-solid fa-car"></i>
-          <i class="fa-solid fa-message"></i>
-          <i class="fa-solid fa-circle-user"></i>
-          <button className = "btn">Վաճառել</button>
+          <i className="fa-solid fa-message" onClick={showSideBarM}></i>
+          <button className="btn">Վաճառել</button>
+        </div>
+         
+        <div className={openM ? "massageBar open" : "massageBar"}>
+          <div className="content">
+          <i className="fa-solid fa-message"></i>
+          <h3>Հարգելի օգտատեր, հաղորդագրություներ ուղարկելու կամ ստանալու համար անհրաժեշտ է մուտք գործել ձեր հաշիվ և հաստատել հեռախոսի համարը</h3>
+          <button className="btn">Մուտք գործել</button>
+          </div>
+          
         </div>
     </div>
    
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,26 +3,40 @@ import {NavLink as Link} from 'react-router-dom'
 import { useState } from 'react';
 import { useEffect } from 'react';
 
+
 const Navbar = () => {
  const [open, setOpen] = useState(false)
- const showSideBar = () => setOpen(true)
  
+ 
+ const showSideBar = () => setOpen(true)
+ const closeSideBar = () => setOpen(false)
+
+
  useEffect(() => {
-  document.addEventListener("mousedown", () =>{
-    setOpen(false)
-  })
+  let handler =  (event) =>{
+    if (event.target.className == 'container open') {
+     
+      setOpen(false)
+    }
+
+  }
+  document.addEventListener("mousedown",handler)
+  return () => {
+    document.removeEventListener("mousedown",handler)
+  }
 })
+
+
+
   return (
-    <> 
-    <div className='bars' onClick={showSideBar}>
-      <i class="fa-solid fa-bars"></i>
-      </div>
    
-     
+  <div>
+
+    <div  className='bars' onClick={showSideBar}>
+      <i class="fa-solid fa-bars"></i>
+    </div>
     
-     <div className={open ? "container open" : "container"}>
-     
-    
+     <div  className={open ? "container open" : "container"}>
 
      <nav className="navbar">
      <span className='user'>
@@ -30,20 +44,28 @@ const Navbar = () => {
      <h2>Մուտք</h2>
     
      </span>
-     <i class="fa-solid fa-message">հաղորդագրություններ</i>
+    <span className='massage'  onClick={closeSideBar}>
+    <i className="fa-solid fa-message"></i>
     
+   <h5>Հաղորդագրություններ</h5>
+    </span>
+
      <Link to={'/dealers'} className="dillers" >Դիլերներ</Link>
      <Link to={'/be_a_dealer'} className="avtodiller">Դառնալ ավտոդիլեր</Link>
      <Link to={'/advertising'} className="govazd">Գովազդ կայքում</Link>
      <Link to={'/terms_of_use'} className="kanon">Օգտագործման կանոնները</Link>
      <Link to={'/help'} className="help">Օգնություն</Link>
      <Link to={'/contact'} className="call">Կապ</Link>  
+
    </nav>
    
-   </div>
+     
+     </div>
+
+  </div>
 
    
-    </>
+   
     
   )
 
