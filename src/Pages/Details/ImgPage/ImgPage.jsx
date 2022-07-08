a694d92a15d54e29122f4c044b5b310fcd28cc92
Gevorg-11 <gevs11@mail.ru>
2022-07-09 01:14:50 +0400

    slide

--- src/Pages/Details/ImgPage/ImgPage.jsx
+++ src/Pages/Details/ImgPage/ImgPage.jsx
@@ -26,17 +26,27 @@ const ImgPage = ({car}) => {
             }
         }
     }
-    const toRight=()=>{
-        for(let i=0;i<car.length;i++){
-            setImg(car[i]);
-            setCount(i+1);
-        }
+    const toRight = () => {
+        let lastIndex = 0
+        img && (lastIndex = car.indexOf(img))
+        setImg(car[lastIndex+1])
+        setCount(count + 1)
+        if(count === car.length) setCount(1);
+    }
+    const toLeft = () => {
+        let lastIndex=0;
+        img && (lastIndex = car.indexOf(img))
+        if(lastIndex===0){
+            setImg(car[car.length-1])
+        }else setImg(car[lastIndex-1])
+        setCount(count - 1)
+        if(count === 1) setCount(car.length);
     }
   return (
     <div className="ImgPage">
     {
         car?<div className="mainImg">
-            <i className='icon left'><FiChevronLeft/></i>
+            <i className='icon left' onClick={()=>toLeft()}><FiChevronLeft/></i>
             <i className='icon right' onClick={()=>toRight()} ><FiChevronRight/></i>
             <i className='react heart'><BsHeartFill/></i>
             <i className="react icon fullscr"><BsArrowsFullscreen/></i>
