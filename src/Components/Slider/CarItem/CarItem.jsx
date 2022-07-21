415f03729731a3e6a833909ee6330db4bc51be97
Gevorg-11 <gevs11@mail.ru>
2022-07-21 23:27:11 +0400

    hover

--- src/Components/Slider/CarItem/CarItem.jsx
+++ src/Components/Slider/CarItem/CarItem.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import { NavLink as Link } from 'react-router-dom'
 
 const CarItem = ({ car }) => {
-
+  const onMouseOver=(e)=>{
+    e.target.src=car.imgUrls[1]
+  }
+  const onMouseLeave=(e)=>{
+    e.target.src=car.imgUrls[0]
+  }
   return (
     car && <div className="carItem">
       <div className='obshi'>
-        <img src={car.imgUrls[0]} alt="car_image" />
+      <Link to={'/detail/'+car.id}>
+          <img onMouseOver={onMouseOver} 
+            onMouseLeave={onMouseLeave}
+            src={car.imgUrls[0]} alt='car_image' />
+        </Link>
         <div className="info">
           <div className=' arajTox'>
             <span>{car.mark}</span>
