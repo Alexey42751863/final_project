09fe488bf8f8c47c1ff2e74cd43dba6ae4c6b024
shushan-melkumyan <shushanmelkumyan93@gmail.com>
2022-07-22 08:47:06 +0300

    login-page and baza

--- src/Components/SellCar/SellCar.jsx
+++ src/Components/SellCar/SellCar.jsx
@@ -21,7 +21,12 @@ const SellCar = ({openLogin, setOpenLogin}) => {
       
         <div id="myModal" className={openLogin?"modal open":"modal"}>
          <div className="modal-content">
+          <div className='head'>
+          
+          <span class="close" onClick={()=>setOpenLogin(false)}>&times;</span>
           <h2 className='enter'>Մուտք</h2>
+          </div>
+          
           <span className='about'> <p>Սոց. հաշիվներով </p> </span>
           <div className='btns'>
             <button className='btn'>Facebook</button>
@@ -32,9 +37,12 @@ const SellCar = ({openLogin, setOpenLogin}) => {
             <input type="text" placeholder='էլ․ հասցե'className='input'/>
             <input type="text" placeholder='գաղտնաբառ' className='input'/>
           </div>
+          <div className='user'>
           <input type="checkbox" className='checkmark'></input>
           <span className='remember'>հիշել</span>
           <span className='forgot'>Մոռացե՞լ եք գաղտնաբառը</span>
+          </div>
+          
           <button className='btn1'>Մուտք</button>
           <h4>Չունե՞ք հաշիվ - Գրանցվել</h4>
             </div>
